cb233dec617d64747f784dd30d27a20e03f740a4
CIITSHSDTP52 <john.marrero@ciit.edu.ph>
2024-03-07 10:49:57 +0800

    final
    done

--- Assets/scripts/script.cs
+++ Assets/scripts/script.cs
@@ -1,51 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
-using TMPro;
+using UnityEngine.SceneManagement;
 
-public class script : MonoBehaviour
+public class StartButton : MonoBehaviour
 {
-
-    public GameObject scene_1, scene_2, scene_3;
-    public GameObject stats;
-    public TextMeshProUGUI healthValue, sanityValue;
-    public int healthpoints, sanitypoints;
-
-    public GameObject[] Scenes;
-    public int ActiveScene;
-    // Start is called before the first frame update
-    void Start()
+    public void OnStartButtonClick()
     {
-        Scenes[0].SetActive(true);
-        
-
+        SceneManager.LoadScene("Scene 2"); 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void SceneChnager()
-    {
-        ActiveScene = 1;
-        Scenes[1].SetActive(true);
-        for (int i = 0; i < Scenes.Length; i++)
-        {
-            Scenes[i].SetActive(i == ActiveScene);
-        }
-    }
-    public void StartButton()
-    {
-        //scene_1.SetActive(false);
-        //scene_2.SetActive(true);
-        //stats.SetActive(true);
-        ActiveScene = 1;
-        Scenes[1].SetActive(true);
-        for (int i = 0; i < Scenes.Length; i ++)
-        {
-            Scenes[i].SetActive(i == ActiveScene);
-        }
-    }
-}   
+}
